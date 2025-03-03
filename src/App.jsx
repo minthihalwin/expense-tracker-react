674e4eb93b598d4ab222814038c1f102069a67fe
minthihalwin <minthihalwin.dev@gmail.com>
2025-03-04 02:25:38 +1300

    Feat: add Balance, Header, IncomeExpenses, TransationList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
 
 function App() {
-  Â;
   return (
     <>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <h2>
-        Tracker
+        Expense Tracker
     </h2>
   )
 }
