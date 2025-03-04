03a1ce30859817d85d324a2359c2382ba823e16c
minthihalwin <minthihalwin.dev@gmail.com>
2025-03-04 20:03:45 +1300

    Feat: add draft Global context provider to share the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
-import AddTransaction from "./components/AddTransaction";
-import Balance from "./components/Balance";
-import Header from "./components/Header";
-import IncomeExpenses from "./components/IncomeExpenses";
-import TransactionList from "./components/TransactionList";
+import {
+  AddTransaction,
+  Balance,
+  Header,
+  IncomeExpenses,
+  TransactionList,
+} from "./components";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -15,7 +18,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </>
+    </GlobalProvider>
   );
 }
 
